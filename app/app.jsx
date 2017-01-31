fe7b4b41e3c1831acd29011a989d5c0a18d70af6
amna <amna.haider21@gmail.com>
2017-01-31 20:05:43 +0400

    Added redux file

---
+++ app/app.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+$(document).foundation();
+//App Custom CSS
+require('style!css!sass!ApplicationCustomStyle');
+
+ReactDOM.render(
+  <h1>React Boiler Plate</h1>,
+  document.getElementById('app')
+);
+require('./redux-example.jsx');
